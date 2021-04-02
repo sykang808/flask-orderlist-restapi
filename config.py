f04403e879829452edb8617a36eef7098e5ac15b
Seyong Kang <nettie0917@gmail.com>
2021-04-03 00:58:30 +0900

    fix database endpoint

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # config.py
 DATABASE_CONFIG = {
-    'DB_HOST': 'mysql-orderlist',
+    'DB_HOST': 'microservice-orderlist.cuzsixgwfkkh.us-west-2.rds.amazonaws.com',
     'DB_NAME': 'orderlist',
-    'DB_USER': 'root',
+    'DB_USER': 'admin',
     'DB_PASSWORD': '1q2w3e4r',
     'DB_PORT': 3306
 }
